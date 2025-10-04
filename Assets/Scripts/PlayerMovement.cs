086dfaa4f15bf7552ca0bc42cd1fa0df1c52e55f
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2025-10-04 09:57:43 +0400

    jumping and crouching
    jumping and crouching

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,9 @@ public class Player : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float speed = 12f;
+    [SerializeField] private float sprintSpeed = 24f;
     [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float crouchSpeed = 6f;
 
     [Header("Physics")]
     [SerializeField] private float gravity = -9.81f;
@@ -19,12 +21,25 @@ public class Player : MonoBehaviour
     private Vector3 playerVelocity;
     private bool isGrounded;
 
-    //Сейчас это закомменчено,в будущем понадобится для включения анимаций и звуков
-    //public bool IsMoving { get; private set; }
+    private float originalHeight;
+    private Vector3 originalCenter;
+    private bool isCrouching;
+    private bool isSprinting;
+
+    private enum MovementState { Walking, Sprinting, Crouching }
+    private MovementState currentState;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        originalHeight = controller.height;
+        originalCenter = controller.center;
+    }
+
+    private void Start()
+    {
+        currentState = MovementState.Walking;
+        Debug.Log($"Состояние: Иду, Скорость: {speed}");
     }
 
     void Update()
@@ -36,22 +51,85 @@ public class Player : MonoBehaviour
             playerVelocity.y = -2f;
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            float newHeight = originalHeight / 2f;
+            controller.height = newHeight;
+
+            float heightDifference = originalHeight - newHeight;
+            controller.center = new Vector3(originalCenter.x, originalCenter.y - heightDifference / 2f, originalCenter.z);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching = false;
+            controller.height = originalHeight;
+            controller.center = originalCenter;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         Vector3 moveDirection = transform.right * x + transform.forward * z;
+        bool isTryingToMove = moveDirection.magnitude > 0.1f;
 
-        //IsMoving = moveDirection.magnitude > 0.1f;
+        if (Input.GetKey(KeyCode.LeftShift) && !isCrouching && isTryingToMove)
+        {
+            if (isGrounded)
+            {
+                isSprinting = true;
+            }
+        }
+        else
+        {
+            isSprinting = false;
+        }
 
-        controller.Move(moveDirection * speed * Time.deltaTime);
+        float currentSpeed;
+        MovementState newState;
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (isCrouching)
+        {
+            currentSpeed = crouchSpeed;
+            newState = MovementState.Crouching;
+        }
+        else if (isSprinting)
+        {
+            currentSpeed = sprintSpeed;
+            newState = MovementState.Sprinting;
+        }
+        else
+        {
+            currentSpeed = speed;
+            newState = MovementState.Walking;
+        }
+
+        if (newState != currentState)
+        {
+            currentState = newState;
+            switch (currentState)
+            {
+                case MovementState.Walking:
+                    Debug.Log($"Состояние: Иду, Скорость: {currentSpeed}");
+                    break;
+                case MovementState.Sprinting:
+                    Debug.Log($"Состояние: Бегу, Скорость: {currentSpeed}");
+                    break;
+                case MovementState.Crouching:
+                    Debug.Log($"Состояние: Крадусь, Скорость: {currentSpeed}");
+                    break;
+            }
+        }
+
+        Vector3 horizontalMove = moveDirection * currentSpeed;
+
+        if (Input.GetButtonDown("Jump") && isGrounded && !isCrouching)
         {
             playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         playerVelocity.y += gravity * Time.deltaTime;
 
-        controller.Move(playerVelocity * Time.deltaTime);
+        Vector3 finalMove = horizontalMove + playerVelocity;
+        controller.Move(finalMove * Time.deltaTime);
     }
 }
